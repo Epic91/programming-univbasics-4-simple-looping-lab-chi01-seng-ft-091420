55e17b40fe75b98169f6a4fdb4592a7f61a2b968
Evelyn Hernandez <hernandeze791@gmail.com>
2020-08-27 14:19:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,5 +21,12 @@ array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
 output_array(array)
 
 def return_string_array(array)
+  count = 0
+  while count < array.length do
+
+    count += 1
+  end
+
+  end
 
 end
